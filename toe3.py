42bd67c03140e9c8ff7c9f8e5d40fed1ceff3c36
lubarog13 <lubarog13@gmail.com>
2025-05-29 18:20:13 +0300

    Норм графики

--- toe3.py
+++ toe3.py
@@ -2,12 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 delta = 0.015
-x = np.arange(0.1, 3, delta)
+x = np.arange(0, 2, delta)
 h = lambda t: 10.138* np.exp(-13.45*t) * np.sin(15.78*t)
 h1 = lambda t: 0.37 - 0.37 * np.exp(-13.45*t) * np.cos(15.78*t) - 0.32 * np.exp(-13.45*t) * np.sin(15.78*t)
 i_0 = lambda t: 2*np.sin( 2.5 *np.pi * t ) +2 * np.sin( 2.5 * np.pi * (t-0.4) ) * list(map(lambda x: 0 if x<0.4 else 1, t))
 i_t = lambda t: (-0.375*np.cos( 7.85*t ) + 0.65*np.sin( 7.85*t) + 0.37 * np.exp(-13.45*t)*np.cos( 15.78*t ) - 0.0057*np.exp(-13.45*t)*np.sin( 15.78*t ))* list(map(lambda x: 1 if x<0.4 else 0, t))
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+ax1.grid('on')
+ax2.grid('on')
+ax1.set_xticks(np.arange(0, 2, 0.1), minor=True)
+ax1.set_yticks(np.arange(0, 3.5, 0.1), minor=True)
+ax2.set_xticks(np.arange(0, 2, 0.1), minor=True)
+ax2.set_yticks(np.arange(0, 3.5, 0.1), minor=True)
 ax1.plot(x, h(x), label="h(t)")
 ax1.plot(x, h1(x), label="h1(t)")
 ax2.plot(x, i_0(x), label="i0(t)")
--- toe3_3.py
+++ toe3_3.py
@@ -3,8 +3,19 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = lambda x:  np.abs(31.42/(61.69-np.pow(x,2)) * np.sin(0.2*x))
+a = lambda x:  np.abs(10*np.pi/((6.25*np.pow(np.pi, 2))-np.pow(x,2)) * np.cos(0.2*x))
 
-x = np.arange(0, 6 * math.pi / 0.4, math.pi/0.4)
-plt.plot(x, a(x), label="A")
+x = np.arange(0, 6 * math.pi / 0.4, 0.1)
+f, (ax1) = plt.subplots(1, 1)
+ax1.plot(x, a(x), label="A")
+# print(0.1 * a(0))
+ax1.hlines(y=0.1 * a(0), xmin=0, xmax=6 * math.pi / 0.4, linewidth=1, color='y')
+ax1.plot(20.5, 0.05, '.', color="blue")
+ax1.hlines(y=0, xmin=0, xmax=20.5, linewidth=1, color='blue')
+ax1.vlines(x=20.5, ymin=0, ymax=0.05, linewidth=1, color='blue')
+ax1.text(0.2, 0.05, "Δw", transform=ax1.transAxes, fontsize=12,
+        verticalalignment='top')
+ax1.set_xticks(np.arange(0, 6 * math.pi / 0.4, math.pi / 0.4), labels=["0", "2.5π", "5π", "7.5π", "10π", "12.5π"])
+ax1.grid('on')
+ax1.legend()
 plt.show()
